5530633aa743c2238bbe0a0c0b39247f6fac7f52
Max Fathauer <maxfathauer@gmail.com>
2022-03-17 20:17:41 -0400

    added drag drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,52 @@ var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    var tempArr = [];
 
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
 
 
 // modal was triggered
@@ -82,6 +127,22 @@ $("#task-form-modal .btn-primary").click(function () {
   }
 });
 
+//trash drop table
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function (event, ui) {
+    console.log("over");
+  },
+  out: function (event, ui) {
+    console.log("out");
+  }
+});
+
 $(".list-group").on("click", "p", function () {
   var text = $(this)
     .text()
@@ -93,29 +154,29 @@ $(".list-group").on("click", "p", function () {
   textInput.trigger("focus");
 });
 
-$(".list-group").on("blur", "textarea", function(){
+$(".list-group").on("blur", "textarea", function () {
   var text = $(this)
     .val()
     .trim();
-  
-    var status = $(this)
-      .closet(".this-group")
-      .attr("id")
-      .replace("list-", "");
 
-    var index = $(this)
-      .closet(".list-group-item")
-      .index();
+  var status = $(this)
+    .closet(".this-group")
+    .attr("id")
+    .replace("list-", "");
 
-    var taskP = $("<p>")
-      .addClass("m-1")
-      .text(text);
+  var index = $(this)
+    .closet(".list-group-item")
+    .index();
 
-    $(this).replaceWith(taskP);
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  $(this).replaceWith(taskP);
 });
 
 // due date was clicked
-$(".list-group").on("click", "span", function() {
+$(".list-group").on("click", "span", function () {
   // get current text
   var date = $(this)
     .text()
@@ -135,7 +196,7 @@ $(".list-group").on("click", "span", function() {
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("blur", "input[type='text']", function () {
   // get current text
   var date = $(this)
     .val()
